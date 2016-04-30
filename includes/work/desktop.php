ce2cd8660a9717c97c2975528ce118ff5c1f91df
levarne <levarne.sobotker@gmail.com>
2016-04-30 12:13:37 +0200

    portfolio upload

---
+++ includes/work/desktop.php
@@ -0,0 +1,30 @@
+<a href="http://advertisingweek.eu/" target="_blank" class="mozaik-item col-sm-4 desktopItems onModalClick">
+    <img class="img-responsive center-block" data-src="holder.js/500x500/auto" alt="500x500" src="/images/work/thumbs/adweekeu2016_thumb.jpg" data-holder-rendered="true">
+</a>
+<a href="http://newyork.advertisingweek.com/" target="_blank" class="mozaik-item col-sm-4 desktopItems onModalClick">
+    <img class="img-responsive center-block" data-src="holder.js/500x500/auto" alt="500x500" src="/images/work/thumbs/adweekcom2015_thumb.jpg" data-holder-rendered="true">
+</a>
+<a href="http://2015.advertisingweek.eu/" target="_blank" class="mozaik-item col-sm-4 desktopItems onModalClick">
+    <img class="img-responsive center-block" data-src="holder.js/500x500/auto" alt="500x500" src="/images/work/thumbs/adweekeu2015_thumb.jpg" data-holder-rendered="true">
+</a>
+<a href="https://www.w3award.com/" target="_blank" class="mozaik-item col-sm-4 desktopItems onModalClick">
+    <img class="img-responsive center-block" data-src="holder.js/500x500/auto" alt="500x500" src="/images/work/thumbs/w3c_thumb.jpg" data-holder-rendered="true">
+</a>
+<a href="http://aiva.org/" target="_blank" class="mozaik-item col-sm-4 desktopItems onModalClick">
+    <img class="img-responsive center-block" data-src="holder.js/500x500/auto" alt="500x500" src="/images/work/thumbs/aivaorg_thumb.jpg" data-holder-rendered="true">
+</a>
+<a href="https://creativeleadersretreat.com/" target="_blank" class="mozaik-item col-sm-4 desktopItems onModalClick">
+    <img class="img-responsive center-block" data-src="holder.js/500x500/auto" alt="500x500" src="/images/work/thumbs/creativeleadersretreat_thumb.jpg" data-holder-rendered="true">
+</a>
+<a href="https://www.daveyawards.com/" target="_blank" class="mozaik-item col-sm-4 desktopItems onModalClick">
+    <img class="img-responsive center-block" data-src="holder.js/500x500/auto" alt="500x500" src="/images/work/thumbs/davey_awards_thumb.jpg" data-holder-rendered="true">
+</a>
+<a href="https://www.oneclub.org/" target="_blank" class="mozaik-item col-sm-4 desktopItems onModalClick">
+    <img class="img-responsive center-block" data-src="holder.js/500x500/auto" alt="500x500" src="/images/work/thumbs/oneclub_thumb.jpg" data-holder-rendered="true">
+</a>
+<a href="http://quiver.la/" target="_blank" class="mozaik-item col-sm-4 desktopItems onModalClick">
+    <img class="img-responsive center-block" data-src="holder.js/500x500/auto" alt="500x500" src="/images/work/thumbs/quiver_thumb.jpg" data-holder-rendered="true">
+</a>
+<a href="https://www.tellyawards.com/" target="_blank" class="mozaik-item col-sm-4 desktopItems onModalClick">
+    <img class="img-responsive center-block" data-src="holder.js/500x500/auto" alt="500x500" src="/images/work/thumbs/tellywards_thumbs.jpg" data-holder-rendered="true">
+</a>
